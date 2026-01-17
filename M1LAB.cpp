df1356cc0a1084964a8bd31866b8f79883f4eff9
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-01-17 15:30:27 +0000

    Started M1LAB

---
+++ M1LAB.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1LAB
+// Rodrigua4692
+// 17 JAN 2026
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    string name = "Jane Smith"
+    int apples = 100
+    double pricePerApple = 0.25
+    
+    cout << "Welcome to " << name;
+    cout << " 's apple orchard." << endl;
+    
+}
