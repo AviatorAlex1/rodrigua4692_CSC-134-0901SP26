dee684c06cde5ad531c077c36e36245b2e8cd0d9
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-02-17 02:18:18 +0000

    +

--- M2T2.cpp
+++ M2T2.cpp
@@ -14,7 +14,7 @@ int main(){
     //variables
     string meal_name = "Hawaiian Calzone";
     int     num_meals; 
-    double meal_price = 8.99;//Change
+    double meal_price = 5.99;//Change
     double sub_total; 
     double tax_rate = 0.08; 
     double tip_amount;
@@ -27,6 +27,7 @@ int main(){
     cout << endl;
     cout << "How many would you like? ";
     cin >> num_meals;
+    cout << "Tip amount? (min 0)"
 
     
     // Do math things
