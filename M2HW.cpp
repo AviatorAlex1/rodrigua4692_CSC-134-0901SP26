46bfd3c554ad589b118f23a63fdbe6c30d1abfbb
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-03 04:01:33 +0000

    Added and Started Q2

--- M2HW.cpp
+++ M2HW.cpp
@@ -44,8 +44,42 @@ int main ()
    cout << "Final Balance: $" << FinalBalance << endl;
    cout << endl;
 
+//----------------------------------------------------------------
+//Q2 
+//Variables
+    const double COST_PER_CUBIC_FOOT = 0.30; 
+    const double CHARGE_PER_CUBIC_FOOT = 0.52;
 
-    //Variables
+    double length,
+            width,
+            height,
+            volume,
+            cost,
+            charge,
+            profit;
 
+    cout << setprecision(2) << fixed << showpoint;
+
+    // Have the user enter the crate dimensions.
+    cout << "enter the dimensions of the crate (in feet): \n";
+    cout << "Length:  ";
+    cin >> length;
+    cout << "Width:  ";
+    cin >> width;
+    cout << "Height:  ";
+    cin >> height;
+
+    // Calculate the Crate Volume, and cost to produce it.
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume *CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost; 
+
+    // Display the calculate data
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet. \n";
+    cout << "cost to build:  $" << cost << endl;
+    cout << " Charge to customer: $" << charge << endl;
+    cout << "Profit:  $" << profit << endl;
 
 }
