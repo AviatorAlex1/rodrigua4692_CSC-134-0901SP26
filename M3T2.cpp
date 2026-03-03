7ac0322cbc1f5922268625bcf0f37689c0749535
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-03 04:59:37 +0000

    werkin

--- M3T2.cpp
+++ M3T2.cpp
@@ -5,11 +5,27 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
 
 int main ()
 {
-    
+    cout << "Let's roll some Dice!" << endl;
+    int seed;
+    cout << "What is your LUCKY number? " << endl;
+    cin >> seed;
+
+    srand(seed);
+
+    const int MAX = 6;
+    int roll = rand() % MAX;
+
+    cout << "Your First roll was: " << roll << endl;
+    cout << "and your Second Roll was: " << rand() % MAX;
+    cout << " and the Third Roll was: " << rand() % MAX; 
+
+    cout << endl;
     return 0;
 }
