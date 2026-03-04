63c15b496217429e200de6cfe5a337936e1fcf8e
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-04 03:13:09 +0000

    Started M3LAB2

---
+++ M3LAB2.cpp
@@ -0,0 +1,37 @@
+// CSC 134
+// M3LAB1
+// Rodrigua4692
+// 02 MAR 2026
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int grade;
+
+    cout << "Enter a Number (Whole) from 0 - 100: ";
+    cin >> grade; 
+
+    if (grade < 0 || grade > 100 ) { 
+        cout >> "This is an invalid number, Please enter a number between 0 and 100. ";
+    }
+    else if (grade >= 90 && grade <= 100) { 
+        cout << "This is a Letter Grade of: A " << endl;
+    }
+    else if (grade >= 80 && grade <= 89) { 
+        cout << "This is a Letter Grade of: B " << endl;
+    }
+    else if (grade >= 70 && grade <= 79) { 
+        cout << "This is a Letter Grade of: C " << endl;
+    }
+    else if (grade >= 60 && grade <= 69) { 
+        cout << "This is a Letter Grade of: D "  << endl;
+    }
+    else { 
+        cout << "This is a Letter Grade of: F " << endl;
+    }            
+    cout << endl;
+    return 0;
+}   
