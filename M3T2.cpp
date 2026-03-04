a825b0f5b78da988e6bb96b13f6681bdf1981f89
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-04 02:42:29 +0000

    FInished M3T2

--- M3T2.cpp
+++ M3T2.cpp
@@ -13,23 +13,47 @@ using namespace std;
 int main ()
 {
     cout << "Let's roll some Dice!" << endl;
-    int seed;
-    cout << "What is your LUCKY number? " << endl;
-    cin >> seed;
+    int seed = time(0);
+    //cout << "Your Seed is: " << seed << endl;
+    //cout << "What is your LUCKY number? " << endl;
+    //cin >> seed;
 
     srand(seed);
 
     const int MAX = 6;
-    int roll = rand() % MAX;
+    int roll1, roll2, total;
+    roll1 = (rand() % MAX) + 1;
+    cout << "Your First roll was: " << roll1 << endl;
 
-    roll = (rand() % MAX) + 1;
-    cout << "Your First roll was: " << roll << endl;
+    roll2 = (rand() % MAX) + 1;
+    cout << "and your Second Roll was: " << roll2 << endl;
 
-    roll = (rand() % MAX) + 1;
-    cout << "and your Second Roll was: " << rand() % MAX;
+    total = roll1 + roll2; 
 
-    roll = (rand() % MAX) + 1;
-    cout << " and the Third Roll was: " << rand() % MAX; 
+    cout << "Your total roll is: " << total << endl;
+
+    //Craps
+    //7 and 11 = WIN!!
+    //2 and 12 = GAME OVER!!!
+
+    if (total == 7){
+        cout << "lucky seven! You win!" << endl; 
+    }
+    else if (total == 11) {
+        cout << "Eleven is a Winner!" << endl;
+    }   
+    else if (total == 2) {
+        cout << "Snake eyes! Too bad, you lose." << endl;
+    }
+    else if (total == 3) {
+        cout << "Sorry, Three is unluckly, You lose." << endl;
+    }
+    else if (total == 12) {
+        cout << "Boxcars! Sorry, you lost." << endl;
+    }
+    else { 
+        cout << "Your point is " << total << " but we'll do that part later" << endl;
+    }
 
     cout << endl;
     return 0;
