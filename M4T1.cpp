9b44ce08524d438983c50538e93a3d0c1a773e26
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-10 02:43:19 +0000

    Finish M4T1

--- M4T1.cpp
+++ M4T1.cpp
@@ -12,14 +12,22 @@ int main()
 
     //Variable
     int count = 5;
+
+    int number = 5;
     
-    int i = 0;
     // Start of While Loop
-    while (count < 5) {
+    cout << "=======================" << endl; // Fancy Line also for OCD purposes... 
+    cout << endl; //Just a break in the chain for OCD purposes... 
+    int i = 0;
+    while (i < number) {
         cout << "Hello" << count << endl;
+        i++;
     }
 
     cout << "That's All!" << endl;
+
+    cout << endl; //Just a break in the chain for OCD purposes... 
+    cout << "=======================" << endl; // Fancy Line also for OCD purposes... 
 
     // program 5-6 
 
@@ -35,6 +43,8 @@ int main()
         cout << num << "\t\t" << num * num << endl;
         num++;
     }
+    cout << "=======================" << endl; // Fancy Line also for OCD purposes... 
+    cout << endl; //Just a break in the chain for OCD purposes... 
 
     return 0; // done
 }
