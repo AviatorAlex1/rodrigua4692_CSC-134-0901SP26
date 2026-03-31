32f4a95587e9c2f85fc4e821906a3b7b85534bc7
AviatorAlex1 <rodrigua4692@student.faytechcc.edu>
2026-03-31 23:26:42 +0000

    Started M5T2

---
+++ M5T2.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// M5T2
+// Rodrigua4692
+// 30 MAR 2026
+
+
+#include <iostream>
+using namespace std;
+
+void printResult(int number, int result) {
+    cout << number << " squared=" << result << endl;
+}
+
+int main() {
+    int count = 1;
+    int result; 
+
+    while (count <= 10){
+        result = count * count;
+        //cout << count << "squared = " << result << endl;
+        printResult(count,result);
+        count++;
+
+    }
+}
